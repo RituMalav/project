86b07a406f51950931ef9d8acf58fa07ff12abdb
root-Rohit007 <thakarerohit.rt@gmail.com>
2022-08-23 15:01:00 +0530

    sign in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 
 import SignUp from "./Pages/Sign-in/signup";
+import SignIn from "./Pages/Sign-in/signin";
+
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
